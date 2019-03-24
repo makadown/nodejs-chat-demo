3e4b8f1801ac4015dc3c92811b0d969771c6d6f4
makadown <maseflores79@gmail.com>
2019-03-23 18:24:53 -0600

    Rerepasando los eventos de socket.io. Interesante!

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,10 @@
 var socket = io();
 
-socket.on('connect', function() {
-  console.log('Connected to server');
-/*
-  socket.emit('createMessage', {
-      from: 'Mayito',
-      text: 'Hey, soy mayito!'
-  }); */
+socket.on('countUpdated', (count) => {
+  console.log('The count has been updated! ', count);
 });
 
-socket.on('disconnect', function() {
-  console.log('Disconnected from server');
-});
-
-socket.on('newMessage', function(message) {
-    console.log('newMessage', message);
+document.querySelector('#increment').addEventListener('click', () =>{
+  console.log('clicked!');
+  socket.emit('increment');
 });
--- src/index.js
+++ src/index.js
@@ -11,8 +11,19 @@ var io = socketIO(server);
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+
 io.on('connection', (socket) => {
     console.log('New websocket connection!');
+
+    socket.emit('countUpdated', count);
+    socket.on('increment', () =>{
+        count++;
+        /* socket.emit manda a una conexion especifica */
+        // socket.emit('countUpdated', count);
+        /* io.emit manda a todos los conectados (broadcast) */
+        io.emit('countUpdated', count)
+    });
 });
 
 server.listen( port, () => { console.log(`Server http arriba arribota en puerto ${port}`) });
