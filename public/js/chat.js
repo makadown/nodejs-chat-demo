c8b1b902739537ea7408f9e9b520ee8a2723b7fe
makadown <maseflores79@gmail.com>
2019-03-24 18:20:59 -0600

    Haciendo broadcast de eventos

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,22 @@
 var socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log('The count has been updated! ', count);
+socket.on('message', (mensaje) => {
+  console.log(mensaje);
 });
 
-document.querySelector('#increment').addEventListener('click', () =>{
-  console.log('clicked!');
-  socket.emit('increment');
+document.querySelector('#message-form').addEventListener('submit', (e) =>{
+  // console.log('mensaje enviado!');
+  e.preventDefault(); // para evitar que se haga un refresh.
+  const mensaje = e.target.elements.mensaje.value;  
+  // [e.target.elements.mensaje.value] es lo mismo que [document.querySelector('#mensaje').value]
+  socket.emit('mensajeCliente', mensaje); 
+});
+
+document.querySelector('#mensaje').addEventListener('keyup', function() {
+  var nameInput = document.querySelector('#mensaje').value;
+  if (nameInput != '') {
+      document.querySelector('#enviar').removeAttribute('disabled');
+  } else {
+      document.querySelector('#enviar').setAttribute('disabled', null);
+  }
 });
--- src/index.js
+++ src/index.js
@@ -11,19 +11,26 @@ var io = socketIO(server);
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+/* socket.emit manda a una conexion especifica */
+// socket.emit('countUpdated', count);
+/* io.emit manda a todos los conectados (broadcast) 
+   socket.broadcast.emit envia a todos los conectados excepto al cliente que generó dicho broadcast */
 
 io.on('connection', (socket) => {
-    console.log('New websocket connection!');
+    // console.log('New websocket connection!');
 
-    socket.emit('countUpdated', count);
-    socket.on('increment', () =>{
-        count++;
-        /* socket.emit manda a una conexion especifica */
-        // socket.emit('countUpdated', count);
-        /* io.emit manda a todos los conectados (broadcast) */
-        io.emit('countUpdated', count)
+    socket.emit('message', 'Bienvenido a la app de chat!');
+    socket.broadcast.emit('message', 'Un nuevo usuario se ha conectado...');
+
+    socket.on('mensajeCliente', (mensaje) =>{
+        // console.log('Mensaje recibido: ' + mensaje);
+        io.emit('message', mensaje);
     });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'Un usuario se ha ido...');
+    });
+
 });
 
 server.listen( port, () => { console.log(`Server http arriba arribota en puerto ${port}`) });
